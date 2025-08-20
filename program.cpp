375e501e128575bc27da8ce3dffed63abb3923fe
NCharles101 <ncharles6231@floridapoly.edu>
2025-08-20 18:43:22 +0000

    Created the first c++ program

---
+++ program.cpp
@@ -0,0 +1,19 @@
+/* 
+08/20/25
+
+*/
+#include <iostream>
+using namespace std;
+using std::cout;
+
+int main(void){
+    cout << "Hello World" <<std::endl;
+
+    //cout - console output (object of an iostream class)
+    //<< stream - insertion operator
+    //std - standard namespace
+    //:: - scope resolution operator
+
+    return 0;
+
+}
