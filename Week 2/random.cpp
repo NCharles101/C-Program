77d8e054685422274ada0a5f5a41c1432ebb5a1d
Nerlin Charles <ncharles6231@floridapoly.edu>
2025-08-25 18:48:10 +0000

    random

---
+++ Week 2/random.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <random>
+
+
+
+int main(void){
+    //Generates the seed
+    std::random_device rd;
+
+    //initialize a random number engine with a seed
+    std::mt19937 end(rd());
+
+    //define distribution
+    std::uniform_int_distribution<int> dist(1,100);
+
+    for(int i = 0; i <50; i++){
+
+        std:cout << dist(eng) << std::endl;
+    }
+
+
+
+
+    return 0 
+}
