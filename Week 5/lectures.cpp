d302c8dcd0488324acd11edc6df6f6bd6dd578ba
Nerlin Charles <ncharles6231@floridapoly.edu>
2025-09-19 18:14:33 +0000

    fsfs

--- Week 5/lectures.cpp
+++ Week 5/lectures.cpp
@@ -4,18 +4,22 @@
 // Prototype
 void printLecture(const struct Lecture& lecture);
 struct Lecture createLecture(std::string title, std::string author, int duration);
+
 // Create a structure
 struct Lecture {
 std::string title;
 int duration;
 std::string author;
 };
+
 int main(void) {
 struct Lecture oop_the_best = createLecture("Structures", "Adam", 50);
+
 // Call a function
 printLecture(oop_the_best);
 return 0;
 }
+
 void printLecture(const struct Lecture& lecture) {
 // Access the properties of an object
 std::cout << std::setw(15) << std::left << "Title: " << lecture.title <<
@@ -24,6 +28,7 @@ std::cout << std::setw(15) << std::left << "Duration: " << lecture.duration /
 60 << " minutes"
 << std::endl;
 }
+
 struct Lecture createLecture( std::string title,
 std::string author,
 int duration) {
