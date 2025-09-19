c1b623186aeeccd5e0bfce213a2403a03d62ab75
Nerlin Charles <ncharles6231@floridapoly.edu>
2025-09-19 18:40:40 +0000

    sfsfsf

--- Week 5/route.cpp
+++ Week 5/route.cpp
@@ -1,10 +1,17 @@
 #include <iostream>
+#include <ctime>
+#include <string>
 
 //create a route structure
 typedef struct {
     private:
     std::string source;
     std::string destination;
+    int length;
+
+    void updateLength(void){
+        length = (source.empty() || destination.empty()) ?0 : rand()% 900 + 75;
+    }
 
     public:
     //get functions (getters, accessor functions)
@@ -18,24 +25,30 @@ typedef struct {
     //set function (mutator functions, setters)
     void setSource(std::string new_source){
         source = new_source;
+        updateLength();
     }
 
     void setDestination(std::string new_destination){
         destination = new_destination;
+        updateLength();
     }
 
     void print(void){
-        std::cout << source << " -> " << destination << std::endl;
+        std::cout << source << " -> " << destination << " : " <<  updateLength << std::endl;
     }
 } Route;
 
 int main(void){
+    
     //create an object of route
     Route fall_trip;
 
     fall_trip.setSource("Lakeland");
     fall_trip.setDestination("Paris");
 
+    fall_trip.print();
+
+    fall_trip.setDestination("New York");
     fall_trip.print();
 
     return 0;
