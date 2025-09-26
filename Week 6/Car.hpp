6dfc83c9c3c2db65701fe59efb3a271a5d84a95b
Nerlin Charles <ncharles6231@floridapoly.edu>
2025-09-26 18:35:46 +0000

    fsfsfs

--- Week 6/Car.hpp
+++ Week 6/Car.hpp
@@ -1,5 +1,17 @@
 // Header file (.hpp, .h)
 //description of the class
+/*
+to do list:
+add:
+-mileage: the odometer of the car
+-fuel_capacity: tank capacity of the car
+-fuel_level: current fuel in gallons
+
+methods:
+void refuel(double gallons);
+void drive(double distance); if car has enough fuel to drive the given distance print <car (make,model) is driving!>
+
+*/
 using namespace std;
 #include <string>
 
