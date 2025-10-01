0d9e2bb6db816ff77d4efc4ecccfd3c43fd36774
Nerlin Charles <ncharles6231@floridapoly.edu>
2025-10-01 18:41:32 +0000

    fsfsfS

--- Week 6/main.cpp
+++ Week 6/main.cpp
@@ -1,11 +1,32 @@
 // testing file 
 #include "Car.hpp"
+#include "CarDealer.hpp"
 
 int main(void) {
     //create a car object
     Car ferrari_spider("Ferrari", "Spider", 2021, 16.4);
+    Car ferrari_superGT("Ferrari", "SuperGT",2025, 23.4);
     ferrari_spider.print();
 
+    Car ferrari_f50;
+    ferrari_f50.setMake(Ferrari);
+    ferrari_f50.setModel(f50);
+    ferrari_f50.setYear(2025);
+    ferrari_f50.setMPG(10.5);
+    ferrari_f50.print();
 
+    //check whether the car is efficient
+    cout << '\n' << (isEfficient(ferrari_f50) ? "Efficient" : "Inefficient");
+    //create a car dealer object
+    CarDealer ferrari_lakeland;
+    ferrari_lakeland.addCar(ferrari_f50);
+    ferrari_lakeland.addCar(ferrari_spider);
+    ferrari_lakeland.addCar(ferrari_superGT);
+
+    ferrari_lakeland.showInventory();
     return 0;
+}
+
+bool isEfficient (const Car& car){
+    return (car.getMPG() > 20.0) ? true : false;
 }
