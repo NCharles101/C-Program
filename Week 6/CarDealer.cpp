4930f0828f9bcfc0e91278d5425c7425b69c4005
Nerlin Charles <ncharles6231@floridapoly.edu>
2025-10-03 18:07:10 +0000

    gdgdgd

--- Week 6/CarDealer.cpp
+++ Week 6/CarDealer.cpp
@@ -2,7 +2,10 @@
 #include <iostream>
 
 void CarDealer::showInventory() const {
-
+    for (int i = 0; i < inventory.size(); i++) {
+        inventory[i].print();
+        std::cout << "---------------\n";
+    }
 }
 
 void CarDealer::addCar(const Car& car){
