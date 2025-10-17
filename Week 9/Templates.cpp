9757a99b31f396a25218faf3bee8e69355165ef7
Nerlin Charles <ncharles6231@floridapoly.edu>
2025-10-17 18:50:06 +0000

    fsfsfs

--- Week 9/Templates.cpp
+++ Week 9/Templates.cpp
@@ -10,6 +10,41 @@ T largest(const T& n1, const T& n2, const T& n3);
 template <typename T>
 T findMax(std::vector<T>& v);
 
+//template class 
+template<typename T1, typename T2>
+class Pair {
+    public:
+        Pair(const T1& f, const T2& s) : first(f), second(s) {}
+
+        //getters
+        T1 getFirst() const{
+            return first;
+        }
+        T2 getSecond() const {
+            return second;
+        }
+
+        //setters
+        void setFirst(const T1& first){
+            this->first = first;
+        }
+        void setSecond(const T2& second){
+            this->second = second;
+        }
+
+    private:
+        T1 first;
+        T2 second;
+};
+
+//create a template func that takes two arguements of any kind and returns a pair of those arguements
+template<typename T3>
+Pair<T1, T2> createPair (const T& p1, const T& p2);
+
+
+
+
+
 int main(void) {
 
     int n1 =1 , n2 = 2 , n3 =3;
@@ -26,6 +61,12 @@ int main(void) {
 
     std::cout << findMax(v) << std::endl;
 
+    //create an object of pair class
+    Pair<int, std::string> p(1,"Jelly");
+
+    std::cout << p.getFirst() << " " << p.getSecond() << std::endl;
+
+    std::cout << createPair(100, 8.4)
 
     return 0;
 }
@@ -60,4 +101,9 @@ T findMax(std::vector<T>& v){
     }
     
     return max;
+}
+
+template<typename T3, typename T4>
+Pair<T1, T2> createPair(const T1& f, const T2& g){
+
 }
