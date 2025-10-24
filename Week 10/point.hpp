bd511d200cbff4faf6575fa6404879133e5ea4fe
Nerlin Charles <ncharles6231@floridapoly.edu>
2025-10-24 18:51:48 +0000

    dgdgd

--- Week 10/point.hpp
+++ Week 10/point.hpp
@@ -1,15 +1,19 @@
 //point operator overloading
 #ifndef POINT_H
 #define POINT_H
+#include <string>
 
 class point{
     public:
-        point(int x = 0, int y = 0);
+        point(int x = 0, int y = 0, const char *tag = nullptr);
+        ~point(); // delete
+        point(const point& other); //copy
 
         //logic operator
         bool operator==(const point& other) const;
 
         bool operator!=(const point& other) const;
+
 
         //arithmetic operator
         point operator+(const point& other) const;
@@ -18,14 +22,26 @@ class point{
 
         point operator*(const point& other) const;
 
-        point operator+=(const point& other) const;
+        
+        // augmented arithmetic operator
+        /* not working
+        point& operator+=(const point& other) const;
 
-        point operator-=(const point& other) const;
+        point& operator-=(const point& other) const;
 
-        point operator*=(const point& other) const;
+        point& operator*=(const point& other) const;
+        */
 
+        std::string toString(void) const;
 
+        // unary operator
+        point operator-();
 
+        //increment
+        point operator++();
+        point operator++(int);
+
+        //script operator
         int& operator[](int index);
 
 
@@ -33,6 +49,7 @@ class point{
     private:
     int x;
     int y;
+    char *tag;
 };
 
 
