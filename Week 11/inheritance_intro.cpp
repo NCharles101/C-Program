a328355672aa67ad62a3f8f003672eab788f48b3
Nerlin Charles <ncharles6231@floridapoly.edu>
2025-10-27 18:50:07 +0000

    dssfstrsdfasefsdfew

---
+++ Week 11/inheritance_intro.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+
+class Human{
+    public:
+        std::string name;
+        int age;
+        Human(): name("John Doe"), age(1) { }
+        Human(std::string name, int age): name(name), age(age) {}
+
+        void print (void) const {
+            std::cout << "Hi! My name is " << name  << '\n';
+            std::cout << "I am " << age << '\n';
+        }
+    
+};
+
+class Student : public Human{
+    public:
+        int student_id;
+        Student(std::string name = "John Doe", int age = 1, int student_id = 0) :
+            Human(name, age), student_id(student_id) {}
+        
+        void print(void) const {
+            Human::print();
+            std::cout << "My student ID is " << student_id << std::endl;
+        }
+};
+
+int main(void){
+    Human h("Slim Shady", 52);
+    h.print();
+    Student st("Micheal Jordan", 57, 23);
+    st.print();
+}
